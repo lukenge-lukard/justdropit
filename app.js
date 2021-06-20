6852bbe2906ceda2bb96d90a68ebaf6431584e10
lukenge-lukard <lukengelukard@gmail.com>
2021-06-20 19:27:53 +0300

    upgrade

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 const PORT = process.env.PORT || 3700;
 
 const publicDirectory = path.join(__dirname, "./public");
@@ -8,44 +9,9 @@ app.use(express.static(publicDirectory));
 
 app.set("view engine", "hbs");
 
-app.get("/", (req, res)=>{
-    res.render("index");
-});
-app.get("/story", (req, res)=>{
-    res.render("story");
-});
-app.get("/lovestory", (req, res)=>{
-    res.render("lovestory");
-});
-app.get("/angry", (req, res)=>{
-    res.render("angry");
-});
-app.get("/fear", (req, res)=>{
-    res.render("fear");
-});
-app.get("/believe", (req, res)=>{
-    res.render("believe");
-});
-app.get("/fine", (req, res)=>{
-    res.render("fine");
-});
-app.get("/powerless", (req, res)=>{
-    res.render("powerless");
-});
-app.get("/handle", (req, res)=>{
-    res.render("handle");
-});
-app.get("/fuck", (req, res)=>{
-    res.render("fuck");
-});
-app.get("/lady", (req, res)=>{
-    res.render("lady");
-});
-app.get("/sorry", (req, res)=>{
-    res.render("sorry");
-});
-app.get("/what-do-you-say", (req, res)=>{
-    res.render("what-do-you-say");
-});
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+//Define Routes
+app.use("/", require("./routes/pages"));
 
 app.listen(PORT, ()=> console.log(`Listening on Port ${PORT}`));
---
+++ routes/pages.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res)=>{
+    res.render("index");
+});
+router.get("/story", (req, res)=>{
+    res.render("story");
+});
+router.get("/lovestory", (req, res)=>{
+    res.render("lovestory");
+});
+router.get("/angry", (req, res)=>{
+    res.render("angry");
+});
+router.get("/fear", (req, res)=>{
+    res.render("fear");
+});
+router.get("/believe", (req, res)=>{
+    res.render("believe");
+});
+router.get("/fine", (req, res)=>{
+    res.render("fine");
+});
+router.get("/powerless", (req, res)=>{
+    res.render("powerless");
+});
+router.get("/handle", (req, res)=>{
+    res.render("handle");
+});
+router.get("/fuck", (req, res)=>{
+    res.render("fuck");
+});
+router.get("/lady", (req, res)=>{
+    res.render("lady");
+});
+router.get("/sorry", (req, res)=>{
+    res.render("sorry");
+});
+router.get("/what-do-you-say", (req, res)=>{
+    res.render("what-do-you-say");
+});
+
+module.exports = router;
